ae47c838ebc8b231f6916befbf70b5e233b0b0af
MeganReesA <rees.megan@gmail.com>
2016-05-30 17:15:16 -0400

    fixed my fup

--- project_4/views/js/main.js
+++ project_4/views/js/main.js
@@ -428,6 +428,25 @@ var resizePizzas = function(size) {
     var oldSize = oldWidth / windowWidth;
 
     // Optional TODO: change to 3 sizes? no more xl?
+    // Changes the slider value to a percent width
+    function sizeSwitcher (size) {
+      switch(size) {
+        case "1":
+          return 0.25;
+        case "2":
+          return 0.3333;
+        case "3":
+          return 0.5;
+        default:
+          console.log("bug in sizeSwitcher");
+      }
+    }
+
+    var newSize = sizeSwitcher(size);
+    var dx = (newSize - oldSize) * windowWidth;
+
+    return dx;
+  } */
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
